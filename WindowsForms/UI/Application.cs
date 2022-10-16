449411754bbd98b181058b433e8093f5a17ebf59
StevenJBurns <StevenJBurns@users.noreply.github.com>
2022-10-16 15:29:48 -0600

    chore: indent spacing clean up in Application.cs

--- WindowsForms/UI/Application.cs
+++ WindowsForms/UI/Application.cs
@@ -3,23 +3,27 @@ using System.Windows.Forms;
 using Microsoft.VisualBasic.ApplicationServices;
 
 namespace MAMS.WinForms
-	{
-	public class SingleInstanceApplication : WindowsFormsApplicationBase
-		{
-		public SingleInstanceApplication()
-            { this.IsSingleInstance = true; }
+  {
+  public class SingleInstanceApplication : WindowsFormsApplicationBase
+    {
+    public SingleInstanceApplication()
+      {
+      this.IsSingleInstance = true;
+      }
 
-        protected override void OnCreateMainForm()
-            { this.MainForm = new FormMain(); }
+    protected override void OnCreateMainForm()
+      {
+      this.MainForm = new FormMain();
+      }
 
-        [STAThread]
-        static void Main(string[] args)
-            {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            SingleInstanceApplication app = new SingleInstanceApplication();
-            app.Run(args);
-            }
-		}
-	}
+    [STAThread]
+    static void Main(string[] args)
+      {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      SingleInstanceApplication app = new SingleInstanceApplication();
+      app.Run(args);
+      }
+    }
+  }
 
